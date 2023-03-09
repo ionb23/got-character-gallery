c4a9f947b63f1169c993d1d9c8852e64c234aa44
ionb23 <ion.borziac@gmail.com>
2023-03-09 20:50:05 +0000

    fixing error

--- src/components/Character.js
+++ src/components/Character.js
@@ -12,7 +12,7 @@ const Character = ({ name, imgUrl, birth, death, race, realm, spouse }) => {
         <div>
             <div data-test="component-character"></div>
             <h1 data-test="char-name">{name}</h1>
-            <img data-test="char-img" src={imgUrl}></img>
+            <img data-test="char-img" src={imgUrl} alt="character avatar" ></img>
             <ul data-test="char-list">
                 <li data-test="char-birth">Date of Birth: {birth}</li>
                 <li data-test="char-death">Date of Death: {death}</li>
